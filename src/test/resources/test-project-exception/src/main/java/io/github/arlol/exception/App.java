6b42f7ae29d95426b041ca82ed8362e4a141f261
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-05-01 16:08:26 +0200

    Add test-project-exception

---
+++ src/test/resources/test-project-exception/src/main/java/io/github/arlol/exception/App.java
@@ -0,0 +1,7 @@
+package io.github.arlol.exception;
+
+public class App {
+	public static void main(String[] args) {
+		throw new IllegalArgumentException("Expection ;)");
+	}
+}
--- src/test/resources/test-project-exception/src/main/java/io/github/arlol/print/App.java
+++
@@ -1,7 +0,0 @@
-package io.github.arlol.print;
-
-public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
