91d518b90b4498ab10d747613fb4126092ed0952
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-05-01 16:13:12 +0200

    Add integration test with exception project

--- src/test/java/io/github/arlol/mvnx/MavenExecutorIntegrationTest.java
+++ src/test/java/io/github/arlol/mvnx/MavenExecutorIntegrationTest.java
@@ -1,5 +1,7 @@
 package io.github.arlol.mvnx;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
@@ -7,11 +9,21 @@ import org.junit.jupiter.api.Test;
 public class MavenExecutorIntegrationTest {
 
 	@Test
-	public void test() throws Exception {
-		// Overwrite repositories to ensure offline usage
+	public void testPrint() throws Exception {
 		Path repository = TestPaths.get("maven-repository");
+		// Overwrite repositories to ensure offline usage
 		MavenExecutor.main(new String[] { "io.github.ArloL:print:0.0.1", "--repositories", "http://localhost:62085",
 				"--localRepository", repository.toString() });
+	}
+
+	@Test
+	public void testException() throws Exception {
+		Path repository = TestPaths.get("maven-repository");
+		// Overwrite repositories to ensure offline usage
+		assertThatThrownBy(() -> {
+			MavenExecutor.main(new String[] { "io.github.ArloL:exception:0.0.1", "--repositories",
+					"http://localhost:62085", "--localRepository", repository.toString() });
+		}).hasCause(new IllegalArgumentException("Expection ;)"));
 	}
 
 }
