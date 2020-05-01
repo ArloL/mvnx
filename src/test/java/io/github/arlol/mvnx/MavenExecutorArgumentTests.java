3d5d9b05113fe5d97cd845b2cbec46ae3ec21ae4
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-05-01 15:23:48 +0200

    Use arrays in argument tests

--- src/test/java/io/github/arlol/mvnx/MavenExecutorArgumentTests.java
+++ src/test/java/io/github/arlol/mvnx/MavenExecutorArgumentTests.java
@@ -1,11 +1,9 @@
 package io.github.arlol.mvnx;
 
-import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.nio.file.Paths;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +12,13 @@ public class MavenExecutorArgumentTests {
 	@Test
 	public void testEmptyArguments() {
 		assertThatThrownBy(() -> {
-			parseArguments(emptyList());
+			parseArguments(new String[0]);
 		}).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("Missing artifact identifier");
 	}
 
 	@Test
 	public void testIdentifier() {
-		MavenExecutor executor = parseArguments(List.of("com.github.ArloL:newlinechecker:133576b455"));
+		MavenExecutor executor = parseArguments(new String[] { "com.github.ArloL:newlinechecker:133576b455" });
 		assertThat(executor.groupId).isEqualTo("com.github.ArloL");
 		assertThat(executor.artifactId).isEqualTo("newlinechecker");
 		assertThat(executor.version).isEqualTo("133576b455");
@@ -30,14 +28,14 @@ public class MavenExecutorArgumentTests {
 	public void testMainClass() {
 		String mainClass = "someunknown.MainClass";
 		MavenExecutor executor = parseArguments(
-				List.of("com.github.ArloL:newlinechecker:133576b455", "--mainClass", mainClass));
+				new String[] { "com.github.ArloL:newlinechecker:133576b455", "--mainClass", mainClass });
 		assertThat(executor.mainClass).isEqualTo(mainClass);
 	}
 
 	@Test
 	public void testRepositories() {
 		MavenExecutor executor = parseArguments(
-				List.of("com.github.ArloL:newlinechecker:133576b455", "--repositories", "https://jitpack.io/"));
+				new String[] { "com.github.ArloL:newlinechecker:133576b455", "--repositories", "https://jitpack.io/" });
 		assertThat(executor.repositories).containsExactly("https://jitpack.io/");
 	}
 
@@ -45,19 +43,19 @@ public class MavenExecutorArgumentTests {
 	public void testSettings() {
 		String settings = "/home/runner/settings.xml";
 		MavenExecutor executor = parseArguments(
-				List.of("com.github.ArloL:newlinechecker:133576b455", "--settings", settings));
+				new String[] { "com.github.ArloL:newlinechecker:133576b455", "--settings", settings });
 		assertThat(executor.settingsXml).isEqualTo(Paths.get(settings));
 	}
 
 	@Test
 	public void testPassthroughArguments() {
 		MavenExecutor executor = parseArguments(
-				List.of("com.github.ArloL:newlinechecker:133576b455", "--", "rest-of-arguments"));
+				new String[] { "com.github.ArloL:newlinechecker:133576b455", "--", "rest-of-arguments" });
 		assertThat(executor.passthroughArguments).containsExactly("rest-of-arguments");
 	}
 
-	private MavenExecutor parseArguments(List<String> arguments) {
-		return new MavenExecutor().parseArguments(arguments.toArray(new String[0]));
+	private MavenExecutor parseArguments(String[] arguments) {
+		return new MavenExecutor().parseArguments(arguments);
 	}
 
 }
